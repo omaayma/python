9c66e0f3abbd4e1ec0d4304afdc339ec59c20704
omaayma <omaymazemmouri03@gmail.com>
2025-10-14 11:44:51 +0200

    .

--- boletín5.py
+++ boletín5.py
@@ -1,8 +1,23 @@
+"""
 #Ejercicio1
 import random
 
 numeros = random.sample(range(1, 50), 6)
 print("Números generados:", numeros)
 
+
 #Ejericio2
+num1 = int(input("Escribe el primer número"))
+num2 = int(input("Escribe el segundo número"))
+
+divisores = []
+
+for i in range(1, min(num1, num2) + 1):
+    if num1 % i == 0 and num2 % i == 0:
+        divisores.append(i)
+print("Los divisores comunes:", divisores)
+"""
+
+#Ejercicio3
+
 
